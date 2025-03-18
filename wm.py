8ccd9f7ce01623d89fb16da59adac470212024d2
Divyanshu-2004 <divyanshuhprajapati9@gmail.com>
2025-03-18 08:06:27 +0530

    project

---
+++ wm.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage
+if __name__ == "__main__":
+    input_string = input("Enter a string to reverse: ")
+    print("Reversed string:", reverse_string(input_string))
